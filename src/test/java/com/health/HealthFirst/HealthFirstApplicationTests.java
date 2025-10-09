690053ca7432bdec2d10611834f96693465ab004
Githiyon <githi001@gmail.com>
2025-10-09 20:44:27 +0530

    Initial Commit

---
+++ src/test/java/com/health/HealthFirst/HealthFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health.HealthFirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
