9477a596d0e5121c38e813c604cb0a0f025420ed
Githiyon <githi001@gmail.com>
2025-10-11 02:01:33 +0530

    HealthLog Controller

---
+++ src/main/java/com/health/HealthFirst/controller/HealthLogController.java
@@ -0,0 +1,31 @@
+package com.health.HealthFirst.controller;
+
+import com.health.HealthFirst.model.HealthLog;
+import com.health.HealthFirst.service.HealthLogServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/healthLog")
+public class HealthLogController {
+    @Autowired
+    private HealthLogServiceImpl service;
+
+    public HealthLogController(HealthLogServiceImpl service){
+        this.service = service;
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<Optional<HealthLog>> findHealthLogByUser(@PathVariable Long userId){
+        return new ResponseEntity<>(service.getHealthLogsByUser(userId), HttpStatus.OK);
+    }
+    @PostMapping("/addLogsByUser")
+    public ResponseEntity<HealthLog> addHealthLogByUser(Long userId, HealthLog healthLog){
+        return new ResponseEntity<>(service.addHealthLog(userId,healthLog),HttpStatus.CREATED);
+    }
+
+}
