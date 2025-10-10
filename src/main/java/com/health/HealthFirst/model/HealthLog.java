c70150fa3edd606581600082f5a0587bcb64b1ff
Githiyon <githi001@gmail.com>
2025-10-10 16:05:07 +0530

    HealthLog Model

---
+++ src/main/java/com/health/HealthFirst/model/HealthLog.java
@@ -0,0 +1,73 @@
+package com.health.HealthFirst.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HealthLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private LocalDate date;
+    private int steps;
+    private double caloriesBurned;
+    private double sleepHours;
+    @ManyToOne
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
+
+    public void setSteps(int steps) {
+        this.steps = steps;
+    }
+
+    public double getCaloriesBurned() {
+        return caloriesBurned;
+    }
+
+    public void setCaloriesBurned(double caloriesBurned) {
+        this.caloriesBurned = caloriesBurned;
+    }
+
+    public double getSleepHours() {
+        return sleepHours;
+    }
+
+    public void setSleepHours(double sleepHours) {
+        this.sleepHours = sleepHours;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
